88e26f5213dae8d71648b659865237a661519dfe
Ilya Smirnov <smilya13@gmail.com>
2018-06-04 10:52:54 +0300

    2-module-1-task & 4-module-1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,23 @@
  * @returns {Object}
  */
 function clone (obj) {
+	
+	let cloneObj = {};
+	
+	if (typeof obj !== 'object') return obj;
+	if (obj === null) return null;
+	
+	for (let key in obj) {
+		
+		if (typeof obj[key] === 'object') {
+			cloneObj[key] = clone(obj[key]);
+		}
+		
+		else {
+			cloneObj[key] = obj[key];
+		};
+	};
+	
+	return cloneObj;
+
 }
