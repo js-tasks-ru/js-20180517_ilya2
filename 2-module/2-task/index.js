4c386cf5879837fff67e88bca2b17aeee8427209
Ilya Smirnov <smilya13@gmail.com>
2018-06-06 14:54:33 +0300

    Module 2 task 2 solved

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -5,4 +5,29 @@
  * @returns {Object}
  */
 function find (obj, value) {
+	
+	function findIn(obj, value, path) {
+		
+		for (let key in obj) {
+			if (obj[key] === value) {
+				answ.push(path + key);
+			}
+			if (typeof obj[key] === 'object') {
+				let pathNew = path + key + '.';
+				findIn(obj[key], value, pathNew);
+			}
+			
+		}
+		
+	}
+
+	let answ = [];
+	let path = '';
+	
+	findIn(obj, value, path);
+	
+	if (answ.length == 0) return null;
+	if (answ.length == 1) return answ[0];
+	return answ;
+	
 }
